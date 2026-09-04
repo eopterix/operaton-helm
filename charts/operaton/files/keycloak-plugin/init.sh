f3f0849a772e50a92cacc8f5e2c4b346e037ed71
Eisenack, Frank <278027713+eopterix@users.noreply.github.com>
2026-09-04 17:46:58 +0000

    fix: downgrade

--- charts/operaton/files/keycloak-plugin/init.sh
+++ charts/operaton/files/keycloak-plugin/init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-PLUGIN_VERSION=2.2.0-M1
+#PLUGIN_VERSION=2.2.0-M1
+PLUGIN_VERSION=2.1.0
 SRC_URL=https://repo1.maven.org/maven2/org/operaton/bpm/extension/operaton-keycloak-run/${PLUGIN_VERSION}/operaton-keycloak-run-${PLUGIN_VERSION}.jar
 TARGET_FILE=/operaton/configuration/userlib/operaton-keycloak-run-${PLUGIN_VERSION}.jar
 if [ ! -f "$TARGET_FILE" ]; then
