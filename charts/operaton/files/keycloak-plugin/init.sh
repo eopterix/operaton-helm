ecf1236e0a41715403cc908874b20d0e92efd19c
Eisenack, Frank <278027713+eopterix@users.noreply.github.com>
2026-09-04 11:25:06 +0000

    feat: connect to iam (1st try)

--- charts/operaton/files/keycloak-plugin/init.sh
+++ charts/operaton/files/keycloak-plugin/init.sh
@@ -2,14 +2,15 @@
 PLUGIN_VERSION=2.2.0-M2
 SRC_URL=https://repo1.maven.org/maven2/org/operaton/bpm/extension/operaton-keycloak-run/${PLUGIN_VERSION}/operaton-keycloak-run-${PLUGIN_VERSION}.jar
 TARGET_FILE=/operaton/configuration/userlib/operaton-keycloak-run-${PLUGIN_VERSION}.jar
-if [ -f "$TARGET_FILE" ]; then
-  echo "Keycloak plugin already added."
-  exit 0
+if [ ! -f "$TARGET_FILE" ]; then
+  echo "Keycloak plugin will be added..."
+  curl -o $TARGET_FILE $SRC_URL
+  chmod 644 /operaton/configuration/userlib/operaton-keycloak-run-${PLUGIN_VERSION}.jar
+  echo "Keycloak plugin added."
+else
+  echo "Keycloak plugin already there."B
 fi
-echo "Keycloak plugin will be added..."
-curl -o $TARGET_FILE $SRC_URL
-chmod 644 /operaton/configuration/userlib/operaton-keycloak-run-${PLUGIN_VERSION}.jar
-echo "Keycloak plugin added."
 echo "Starting Operaton..."
 cd /operaton
 ./operaton.sh
+
